e183df87fe8d00cac42d176925c4279d1432e5f6
pwoosam <pwoosam5@icloud.com>
2018-11-05 02:35:10 -0800

    Added bgimage endpoint and assets for bgimages of common sizes

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import * as bodyParser from 'body-parser';
 import * as express from 'express';
 import * as cors from 'cors';
 import * as methodOverride from 'method-override';
+import * as glob from 'glob';
 import { RegisterRoutes } from './routes';
 
 const app = express();
@@ -28,6 +29,32 @@ app.use('/swagger.json', (req, res) => {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(methodOverride());
+
+app.use('/bgimage/:size', (req, res) => {
+  const size = req.params.size;
+  const filenames = glob.sync(`${__dirname}/assets/bgimage-*.jpg`).sort((a, b) => {
+    const aMatches = <string[]>a.match(/[0-9]+/g);
+    const aSize = parseInt(aMatches[0]);
+    const bMatches = <string[]>b.match(/[0-9]+/g);
+    const bSize = parseInt(bMatches[0]);
+
+    return aSize - bSize;
+  });
+  const filename = filenames.find(item => {
+    const matches = <string[]>item.match(/[0-9]+/g);
+    if (matches.length == 1) {
+      if (size <= parseInt(matches[0])) {
+        return true;
+      }
+    }
+    return false;
+  });
+  if (filename) {
+    res.sendFile(filename);
+  } else {
+    res.sendFile(filenames[filenames.length - 1]);
+  }
+});
 
 RegisterRoutes(app);
 
